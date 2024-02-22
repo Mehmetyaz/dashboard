f07cba4910b90e2fa56057c92e6255e031982a82
Menno van Hout <info@mennovanhout.nl>
2024-02-22 14:59:48 +0100

    Remove wrong throw

--- lib/src/controller/dashboard_controller.dart
+++ lib/src/controller/dashboard_controller.dart
@@ -106,7 +106,6 @@ class DashboardItemController<T extends DashboardItem> with ChangeNotifier {
     } else {
       throw Exception("Not Attached");
     }
-    throw 0;
   }
 
   /// Delete an item from Dashboard.
